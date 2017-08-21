98459fe9088fb47406160f71f97980f801e7d196
Jonathan Beckman <jbecks.media@gmail.com>
2017-08-21 17:13:37 -0600

    initial commit, initial boiler-plate build

---
+++ src/Components/Controls/Controls.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Controls = ({ apiCall }) => {
+  return (
+    <div className="buttons">
+      <div onClick={e => apiCall(e.target.innerHTML)} className="people-btn">
+        Peoples
+      </div>
+      <div onClick={e => apiCall(e.target.innerHTML)} className="planet-btn">
+        Planets
+      </div>
+      <div onClick={e => apiCall(e.target.innerHTML)} className="vehicle-btn">
+        Vehicles
+      </div>
+      <div onClick={e => apiCall(e.target.innerHTML)} className="favorite-btn">
+        Favorite
+      </div>
+    </div>
+  );
+};
+
+export default Controls;

a492dac34555b1138b26ef66b9dfa5ba42edd8df
Jason Lucas <jlucas5280@gmail.com>
2017-08-22 13:09:38 -0600

    commit before merge master into branch

--- src/Components/Controls/Controls.js
+++ src/Components/Controls/Controls.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Controls.css";
 import dashboard from '../../assets/dashboard.png';
+import boba from '../../assets/boba.mp4';
 
 const Controls = ({ apiCall }) => {
   return (
@@ -9,22 +10,27 @@ const Controls = ({ apiCall }) => {
       <section className='controls-container'>
         <img className='dashboard' src={ dashboard } />
 
+        <section className="button-container">
+          <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
+            <p className='button-title'>PEOPLE</p>
+          </div>
+          <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
+            <p className='button-title'>PLANETS</p>
+          </div>
+          <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
+            <p className='button-title'>VEHICLES</p>
+          </div>
+          <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
+            <p className='button-title'>FAVORITES</p>
+          </div>
+        </section>
 
-
-          <section className="button-container">
-            <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
-              <p className='button-title'>PEOPLE</p>
-            </div>
-            <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
-              <p className='button-title'>PLANETS</p>
-            </div>
-            <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
-              <p className='button-title'>VEHICLES</p>
-            </div>
-            <div className='button' onClick={e => apiCall(e.target.innerHTML)}>
-              <p className='button-title'>FAVORITES</p>
-            </div>
-          </section>
+        <section className='boba-container'>
+          <video poster={ boba } className='boba-video' playsInline autoPlay muted loop>
+              <source src={ boba } type='video/webm'></source>
+              <source src={ boba } type='video/mp4'></source>
+          </video>
+        </section>
 
 
 
